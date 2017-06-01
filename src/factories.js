c2d52f92ef539714915d6a5a5eb502af1634f6e8
pdf <pedrodiasfonseca@gmail.com>
2017-06-01 00:00:18 -0300

    Remove some factories from showcase.

--- src/factories.js
+++ src/factories.js
@@ -1,24 +1,18 @@
 import bocaDaMata from './data/boca-da-mata';
-import decorbras from './data/decorbras';
 import design from './data/design';
 import dinhos from './data/dinhos';
-import dmobile from './data/dmobile';
 import johnatas from './data/johnatas';
 import manos from './data/manos';
-import marteletto from './data/marteletto';
 import millenium from './data/millenium';
 import prisma from './data/prisma';
 import wagnelle from './data/wagnelle';
 
 const factories = {
     bocaDaMata,
-    decorbras,
     design,
     dinhos,
-    dmobile,
     johnatas,
     manos,
-    marteletto,
     millenium,
     prisma,
     wagnelle,
