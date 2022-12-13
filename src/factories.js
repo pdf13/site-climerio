6fb7ac0c954952c16d108d9bbd687a5625841787
pdf <pedrodiasfonseca@gmail.com>
2022-12-13 20:16:22 -0300

    add prisma and wagnelle factories

--- src/factories.js
+++ src/factories.js
@@ -3,13 +3,17 @@ import bocaDaMata from './data/boca-da-mata';
 import oldBlackDesign from './data/old-black-design';
 import johnatas from './data/johnatas';
 import millenium from './data/millenium';
+import prisma from './data/prisma';
+import wagnelle from './data/wagnelle';
 
 const factories = {
     alphaDinhos,
     bocaDaMata,
     johnatas,
     millenium,
+    prisma,
     oldBlackDesign,
+    wagnelle,
 }
 
 export default factories;
