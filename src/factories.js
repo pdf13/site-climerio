c0f8ea25f76ab7d80a4b98490aab105d180148c6
pdf <pedrodiasfonseca@gmail.com>
2024-03-31 19:30:04 -0300

    Remove old photos and compnies, and add new catalogues

--- src/factories.js
+++ src/factories.js
@@ -1,19 +1,13 @@
 import alphaDinhos from './data/alpha-dinhos';
 import bocaDaMata from './data/boca-da-mata';
-import oldBlackDesign from './data/old-black-design';
 import johnatas from './data/johnatas';
 import millenium from './data/millenium';
-import prisma from './data/prisma';
-import wagnelle from './data/wagnelle';
 
 const factories = {
     alphaDinhos,
     bocaDaMata,
     johnatas,
     millenium,
-    prisma,
-    oldBlackDesign,
-    wagnelle,
 }
 
 export default factories;
