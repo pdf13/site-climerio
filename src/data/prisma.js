3aa877a0c500d0a2f7e5ea1057388683b04b919f
pdf <pdf@MacBook-Air-de-pdf.local>
2025-10-15 08:28:11 -0300

    Update photos

---
+++ src/data/prisma.js
@@ -0,0 +1,17 @@
+const data = {
+    url: '/prisma',
+    slug: 'prisma',
+    name: 'Prisma',
+    photos: [
+        'prisma-001.jpeg',
+        'prisma-002.jpeg',
+        'prisma-003.jpeg',
+        'prisma-004.jpeg',
+        'prisma-005.jpeg',
+        'prisma-006.jpeg',
+        'prisma-007.jpeg',
+        'prisma-008.jpeg'
+    ]
+};
+
+export default data;
