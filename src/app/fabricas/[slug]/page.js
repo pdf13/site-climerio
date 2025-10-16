31b8e3f90b6a23dd438f08ae80caaf18da366950
pdf (aka Pedro) <pedrodiasfonseca@gmail.com>
2025-10-16 09:54:49 -0300

    Boilerplate for new site structure (#36)
    * Boilerplate for new site structure
    
    * Linha aparador TESTE
    
    * teste
    
    * Teste produto colchoart
    
    * Remove product from list
    
    * Aula 2 dia 06
    
    * .
    
    * teste nome
    
    * teste para jonas
    
    * Jonas 2 teste Paonanda
    
    * barra
    
    * Fix json file
    
    * Add LeA products, and generate unique URL for opening the photo
    
    * Add some categories for products
    
    * Fix wrong category
    
    * Add new product photos
    
    * Add missing sofa
    
    * Add more factories
    
    * Rename files
    
    * v2.0
    
    * v2.0 - final adjustments
    
    ---------
    
    Co-authored-by: jonas-63 <jonasfonsecavendas@gmail.com>

---
+++ src/app/fabricas/[slug]/page.js
@@ -0,0 +1,35 @@
+'use client';
+
+import 'react-photo-view/dist/react-photo-view.css';
+
+import data from '../../../data/factory.json';
+import { notFound } from 'next/navigation';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import { useParams } from 'next/navigation';
+import Image from 'next/image'; // TODO: use latter to optimize
+
+export default function FabricDetail() {
+    const params = useParams()
+    const slug = params.slug
+
+    const factory = data.find((factory) => factory.slug === slug);
+
+    if (!factory) {
+        return notFound();
+    }
+
+    const Item = ({image}) =>
+        <PhotoView src={image}><img src={image} className="shadow-lg mb-8 rounded-xl"/></PhotoView>
+
+    return (
+        <article className='prose mb-8 max-w-none'>
+            <h1>{factory.name}</h1>
+
+            {factory.description && <p>{factory.description}</p>}
+
+            <PhotoProvider>
+                {factory.images.map((image, index) => <Item image={image} index={index} key={index}/>)}
+            </PhotoProvider>
+        </article>
+    )
+}
